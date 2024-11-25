f8a33025a813cf3b30c8c4781b5afeb22219204e
KamilaGrabowska <dudekkamila@icloud.com>
2024-11-25 17:50:44 +0100

    added title of post from JsonServer

--- app-router/src/app/posts/page.tsx
+++ app-router/src/app/posts/page.tsx
@@ -1,7 +1,27 @@
+import {Posts} from "@/types/Posts";
+import style from "@//app/posts/posts.module.scss";
+import {commonMetadata} from '@/common/shared-metadata';
+
+export const metadata= {
+    title: `Posts ${commonMetadata.title}`,
+    description: "Posts page",
+};
+
 export default async function PostsPage(){
+    const res = await fetch("http://localhost:3004/posts");
+
+    if (!res.ok) {
+        throw new Error("Problem with the posts");
+    }
+
+    const posts: Posts = await res.json();
+
     return (
         <div>
             <h1>Posts</h1>
+            {posts.map((posts)=> (
+                <div className={style.item} key={posts.id}>{posts.title}</div>
+            ))}
         </div>
     );
 }
---
+++ app-router/src/app/types/Posts.ts
@@ -0,0 +1,12 @@
+export type Post = {
+    id: number,
+    title: string,
+    body: string,
+    userId: number,
+    tags: string[],
+    reactions:number,
+};
+
+export type Posts ={
+    posts: Post[],
+};
